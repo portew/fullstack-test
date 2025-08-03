c574855cfc2db60e1b6e07e8224635d54ca83067
Mikołaj Kawczyński <mkawczynski@mkawczynski.pl>
2025-08-03 14:26:38 +0200

    Poprawa wcięcia w kodzie.

--- app/Models/ReceiptRegistration.php
+++ app/Models/ReceiptRegistration.php
@@ -8,44 +8,44 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ReceiptRegistration extends Model
 {
-    protected $fillable = [
-        'participant_name',
-        'email',
-        'phone',
+  protected $fillable = [
+    'participant_name',
+    'email',
+    'phone',
     'store_id',
-        'receipt_number',
-        'purchase_date',
-        'receipt_image_path',
-        'contest_answer',
-        'consent_regulations',
-        'consent_rodo',
-        'consent_external',
-        'referrer',
-        'landing_url',
-        'ip_address'
-    ];
+    'receipt_number',
+    'purchase_date',
+    'receipt_image_path',
+    'contest_answer',
+    'consent_regulations',
+    'consent_rodo',
+    'consent_external',
+    'referrer',
+    'landing_url',
+    'ip_address'
+  ];
 
-    protected $casts = [
-        'purchase_date' => 'date',
-        'consent_regulations' => 'boolean',
-        'consent_rodo' => 'boolean',
-        'consent_external' => 'boolean',
-    ];
+  protected $casts = [
+    'purchase_date' => 'date',
+    'consent_regulations' => 'boolean',
+    'consent_rodo' => 'boolean',
+    'consent_external' => 'boolean',
+  ];
 
-    public function dailyRankings() : hasMany
-    {
-        return $this->hasMany(DailyRanking::class, 'submission_id');
-    }
+  public function dailyRankings(): hasMany
+  {
+    return $this->hasMany(DailyRanking::class, 'submission_id');
+  }
 
-    public function finalAward() : hasOne
-    {
-        return $this->hasOne(FinalAward::class, 'submission_id');
-    }
+  public function finalAward(): hasOne
+  {
+    return $this->hasOne(FinalAward::class, 'submission_id');
+  }
 
-    public function winnerContactRequest()
-    {
-        return $this->hasOne(WinnerContactRequest::class, 'submission_id');
-    }
+  public function winnerContactRequest()
+  {
+    return $this->hasOne(WinnerContactRequest::class, 'submission_id');
+  }
 
   public static function winners()
   {
